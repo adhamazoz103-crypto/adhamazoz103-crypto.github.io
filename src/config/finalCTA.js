b359e07d0122fc13e2adb1764389821560b76b72
adhamazoz103-crypto <adhamazoz103@gmail.com>
2026-08-09 01:17:58 +0300

    Revert "Initial commit"
    This reverts commit 02e8a7ab4d613295849c0ea6a28d8ae19d23c9b8.

--- src/config/finalCTA.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.finalCTAConfig = void 0;
-exports.finalCTAConfig = {
-    headline: 'ابدأ رحلتك القانونية بثقة.',
-    description: 'دع فريق GPS للمحاماة والاستشارات القانونية يساعدك في الوصول إلى الحل القانوني المناسب وحماية حقوقك.',
-    buttonLabel: 'احجز استشارتك الآن',
-    buttonHref: '#contact',
-};
